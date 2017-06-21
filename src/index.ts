f2e36c478e8015710a90ad586d60b6b1f853984b
Mark Knight <dev@markfknight.com>
2017-06-21 23:58:05 +0100

    bug(index): fix merge confilct

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ import * as process from 'process';
 
 export class Startup {
   public static main(argv: string[]): void {
-<<<<<<< HEAD
     readNumberOfNewlinesAsyc(argv[2], (data: number) => {console.log(data)});
-=======
-    console.log(linesInFile(argv[2]));
->>>>>>> 09d2314c2c47be71a89b0e0d80edd3f9a8821125
   }
 }
 
