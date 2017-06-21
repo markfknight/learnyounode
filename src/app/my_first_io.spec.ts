16336e4521420d63bc0d98b492a1c045b58e2b29
Mark Knight <dev@markfknight.com>
2017-06-21 23:54:31 +0100

    feat(io): test spec

---
+++ src/app/my_first_io.spec.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import { linesInFile } from './my_first_io';
+
+const fileName = `${__dirname}/my_first_io_test_file.txt`;
+
+beforeAll(() => {
+    fs.writeFileSync(
+        fileName ,
+        'This is a test file.\nWith one newline.' ,
+        (err) => {
+            if (err) { throw err };
+            console.log('my_first_io test file created!');
+        })
+});
+
+afterAll(() => fs.unlinkSync(fileName));
+
+test('Reads two one newline', () => {
+    expect(linesInFile(fileName)).toBe(1);
+});
